aceace44a9dda8248f80767d157ae577e6865b51
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2022-10-20 15:03:45 +0900

    multiple cantainers running

--- .bashrc
+++ .bashrc
@@ -5,33 +5,6 @@ eval $(dircolors -b ~/.dircolors)
 #error color scheme
 color()(set -o pipefail;"$@" 2> >(sed $'s,.*,\e[31m&\e[m,'>&2))
 
-#alias
-alias ..='cd ..'
-alias ls='ls --color'
-alias ll='ls -alh --color'
-alias rm='rm -rfi'
-alias vi='vim'
-alias python='color python3'
-alias ta='tmux a -t 0'
-
-alias py38='sh ~/start.sh -py38'
-alias conda='sh ~/start.sh -conda'
-alias jupyter='sh ~/start.sh -jupyter'
-alias gpu='sh ~/start.sh -gpu'
-alias down='sh ~/start.sh -down'
-
-alias gs='git status'
-alias gb='git branch'
-alias gc='git checkout'
-alias gau='git add -u' #変更されたfileをadd
-alias gcm='git commit -m'
-alias gpsoh='git push -u origin HEAD' #現在のbranchの最新のcommitをpush
-
-alias dps='docker ps'
-alias dimg='docker images'
-alias drm='docker rm -f'
-alias drmi='docker rmi -f'
-
 #power shell
 eval "$(oh-my-posh-wsl --init --shell bash --config /mnt/c/Users/hasegawa/AppData/Local/Programs/oh-my-posh/themes/iterm2.omp.json)"
 
@@ -50,3 +23,33 @@ export NVM_DIR="$HOME/.nvm"
 
 #locale setting
 export LANG=en_US.utf8
+
+#alias
+alias ..='cd ..'
+alias ls='ls --color'
+alias ll='ls -alh --color'
+alias rm='rm -rfi'
+alias vi='vim'
+alias python='color python3'
+
+alias ta='tmux a -t 0'
+
+alias bs='cat ~/.bashrc'
+
+alias py38='sh ~/start.sh -py38'
+alias conda='sh ~/start.sh -conda'
+alias jupyter='sh ~/start.sh -jupyter'
+alias gpu='sh ~/start.sh -gpu'
+alias down='sh ~/start.sh -down'
+
+alias gs='git status'
+alias gb='git branch'
+alias gc='git checkout'
+alias gau='git add -u' #変更されたfileをadd
+alias gcm='git commit -m'
+alias gpsoh='git push -u origin HEAD' #現在のbranchの最新のcommitをpush
+
+alias dps='docker ps'
+alias dimg='docker images'
+alias drm='docker rm -f'
+alias drmi='docker rmi -f'
--- start.sh
+++ start.sh
@@ -17,67 +17,59 @@ jupyter=jupyter
 if [ $1 = $pyrun ]; then
     cd ~/docker-python/.devcontainer
     if [ "$containeridpy38" != $py38 ]; then
-        echo "py38 built"
-        docker compose up -d --remove-orphans
+        docker compose up -d
     else
-        echo "py38 already exit"
+        echo "py38 already running"
     fi
     echo "py38 activated"
     docker compose exec py38 bash
 elif [ $1 = $condarun ]; then
     cd ~/docker-conda/.devcontainer
     if [ "$containeridconda" != $conda ]; then
-        echo "conda built"
-        docker compose up -d --remove-orphans
+        docker compose up -d
     else
-        echo "conda already exit"
+        echo "conda already running"
     fi
     echo "conda activated"
     docker compose exec conda bash
 elif [ $1 = $gpurun ]; then
     cd ~/docker-gpu/.devcontainer
     if [ "$containeridgpu" != $gpu ]; then
-        echo "gpu built"
-        docker compose up -d --remove-orphans
+        docker compose up -d
     else
-        echo "gpu already exit"
+        echo "gpu already running"
     fi
     echo "gpu activated"
     docker compose exec gpu bash
 elif [ $1 = $jupyterrun ]; then
     cd ~/docker-jupyter/.devcontainer
     if [ "$containeridjupyter" != $jupyter ]; then
-        echo "jupyter built"
-        docker compose up -d --remove-orphans
+        docker compose up -d
     else
-        echo "jupyter already exit"
+        echo "jupyter already running"
     fi
     echo "jupyter activated"
     docker compose exec jupyter bash
 elif [ $1 = $down ]; then
     if [ "$containeridpy38" = $py38 ]; then
         cd ~/docker-python/.devcontainer
-        echo "py38 deactivated"
-        docker compose down
+        docker compose down --remove-orphans
     elif [ "$containeridconda" = $conda ]; then
         cd ~/docker-conda/.devcontainer
-        echo "conda deactivated"
-        docker compose down
+        docker compose down --remove-orphans
     elif [ "$containeridgpu" = $gpu ]; then
         cd ~/docker-gpu/.devcontainer
-        echo "gpu deactivated"
-        docker compose down
+        docker compose down --remove-orphans
     elif [ "$containeridgpu" = $jupyter ]; then
         cd ~/docker-jupyter/.devcontainer
-        echo "jupyter deactivated"
-        docker compose down
+        docker compose down --remove-orphans
     fi
 else
     echo "ERROR"
+    echo "available_options"
     echo "-py38"
     echo "-conda"
     echo "-gpu"
-    echo "-jpyter"
+    echo "-jupyter"
     echo "-down"
-    echo "available_option"
 fi
